34cda705a3669ec6f4a99fcb7294499d379856c2
Kolesnikova_Daria <daria.kolesnikova@ru.axxonsoft.com>
2024-03-18 12:11:14 +0300

    add dll run

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, redirect, url_for, render_template, request
 import os
 import subprocess
+from ctypes import *
+import ctypes
 
 app = Flask(__name__)
 
@@ -25,10 +27,36 @@ def user(usr):
 def exe():
     if request.method == "POST":
         if request.form['submit'] == 'runFile':
-            text = "... running ..."
-            filepath="MINA.exe"
-            subprocess.call(["missile/MINA.exe"])
-            return render_template('assemblies.html', text=text)
+            current_dir = os.getcwd()
+            Missile = WinDLL(current_dir + "/Missile-dll.dll")
+            Missile.MissileModelling.argtypes = [
+                ctypes.c_double,  # x0
+                ctypes.c_double,  # y0
+                ctypes.c_double,  # v0
+                ctypes.c_double,  # tet0
+                ctypes.c_double,  # mass
+                ctypes.c_double,  # cx
+                ctypes.c_double,  # d
+                ctypes.POINTER(ctypes.c_double),  # outX
+                ctypes.POINTER(ctypes.c_double),  # outY
+                ctypes.POINTER(ctypes.c_int)  # size
+            ]
+            size = ctypes.c_int()
+    
+            # Assuming a maximum size for the output arrays
+            max_size = 1000
+            outX = (ctypes.c_double * max_size)()
+            outY = (ctypes.c_double * max_size)()
+            
+            x0, y0, v0, tet0, mass, cx, d = 0.0, 0.0, 100.0, 45.0, 10.0, 0.1, 0.5
+            Missile.MissileModelling(x0, y0, v0, tet0, mass, cx, d, outX, outY, ctypes.byref(size))
+            
+            # Convert the C arrays to Python lists
+            outX_list = [outX[i] for i in range(size.value)]
+            outY_list = [outY[i] for i in range(size.value)]
+            print(outX_list, outY_list)
+            
+            return render_template('assemblies.html', text=outX_list[3])
     else:
         return render_template("assemblies.html")
 
